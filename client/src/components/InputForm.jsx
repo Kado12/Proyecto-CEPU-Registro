16d404fab9afa643317892cc36e8512d34307f33
Kado12 <gonzalosh01@gmail.com>
2024-11-15 13:55:56 -0500

    Delete required

--- client/src/components/InputForm.jsx
+++ client/src/components/InputForm.jsx
@@ -3,7 +3,7 @@ import './InputForm.css'
 const InputForm = ({ type, name, value, onChange, placeholder }) => {
   return (
     <div className="group-input-form">
-      <input required type={type} className="input-form" name={name} value={value} onChange={onChange} />
+      <input type={type} className="input-form" name={name} value={value} onChange={onChange} />
       <span className="highlight" />
       <span className="bar" />
       <label>{placeholder}</label>
